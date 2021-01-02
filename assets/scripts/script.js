bd73a07ab8f9e521fd1dcb7c647781d058c06b5d
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-01-02 13:44:49 -0500

    Added content to sidebar.   Added event listener/ funciton for opening sidebar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
     console.log("hello world");
 
 
+    function showSidebar() {
+        console.log("clicked");
+        $(".sidebar").css("width","200px");
+        //$("#main").css("margin-left","250px");
+    }
 
+    $("#hamburger-open").on("click", showSidebar);
 
 
 }); 
