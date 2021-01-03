3685342972a83350919e8eb851fba4519cf38384
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-01-03 13:26:57 -0500

    Created function to display card to portfolio page

---
+++ assets/scripts/portfolio.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  // JAVASCRIPT VARIABLES
+  var project = {
+    name: "",
+    description: "",
+    technologies: [],
+    repository: "",
+    deployed: "",
+    image: "",
+    imageAlt: "",
+    gif: "",
+  };
+  var projects = [
+    {
+      name: "What's in the Box?",
+      description:
+        "An application for tracking the context of boxes packed for household moving and storage projects",
+      technologies: [],
+      repository: "https://github.com/pkriengsiri/whats-in-the-box",
+      deployed: "https://github.com/pkriengsiri/whats-in-the-box",
+      image: "./assets/images/box.png",
+      imageAlt: "screenshot of What's in the Box? application",
+      gif: "",
+    },
+  ];
+
+  // FUNCTION DEFINITIONS
+
+  // Creates a project card and appends it to the DOM
+  function createProjectCards() {
+    for (var i = 0; i < projects.length(); i++) {
+      var cardDiv = $("<div>").addClass("card card-custom align-items-center");
+      cardDiv.attr("data-project", i);
+      var imgEl = $("<img>").addClass("card-img-top");
+      imgEl.attr("src", projects[0].image);
+      imgEl.attr("alt", projects[0].imageAlt);
+      var cardBody = $("<div>").addClass("card-body");
+      var cardTitle = $("<h3>").addClass("card-title");
+      cardTitle.text(projects[0].name);
+      cardBody.append(cardTitle);
+      cardDiv.append(imgEl);
+      cardDiv.append(cardBody);
+      $("#project-list").append(cardDiv);
+    }
+  }
+
+  // FUNCTION CALLS
+  createProjectCards();
+
+  // EVENT LISTENERS
+});
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,18 @@
-$(document).ready(function() {
-    console.log("hello world");
+$(document).ready(function () {
+  // FUNCTION DEFINITIONS
+  // Hides the sidebar
+  function closeSidebar() {
+    $(".sidebar").css("width", "0px");
+  }
 
+  // Makes the sidebar appear
+  function showSidebar() {
+    console.log("clicked");
+    $(".sidebar").css("width", "160px");
+    //$("#main").css("margin-left","250px");
+  }
 
-    function closeSidebar() {
-        $(".sidebar").css("width","0px");
-    }
-    
-    function showSidebar() {
-        console.log("clicked");
-        $(".sidebar").css("width","160px");
-        //$("#main").css("margin-left","250px");
-    }
-
-    $("#hamburger-open").on("click", showSidebar);
-    $("#hamburger-close").on("click", closeSidebar);
-
-
-}); 
+  // EVENT LISTENERS
+  $("#hamburger-open").on("click", showSidebar);
+  $("#hamburger-close").on("click", closeSidebar);
+});
